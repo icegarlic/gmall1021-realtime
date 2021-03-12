79b3eff5eba20279360976cabe85148a299627d8
zhaohan <577530562@qq.com>
2021-03-12 12:01:02 +0800

    update

--- gmall1021-sparkstreaming/src/main/scala/com/atguigu/gmall1021/realtime/app/DauApp.scala
+++ gmall1021-sparkstreaming/src/main/scala/com/atguigu/gmall1021/realtime/app/DauApp.scala
@@ -1,10 +1,17 @@
 package com.atguigu.gmall1021.realtime.app
 
-import com.atguigu.gmall1021.realtime.util.MyKafkaUtil
+import com.alibaba.fastjson.{JSON, JSONObject}
+import com.atguigu.gmall1021.realtime.util.{MyKafkaUtil, RedisUtil}
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+import redis.clients.jedis.Jedis
+
+import java.lang
+import java.text.SimpleDateFormat
+import java.util.Date
+import scala.collection.mutable.ListBuffer
 
 object DauApp {
 
@@ -16,13 +23,81 @@ object DauApp {
     val topic = "ODS_BASE_LOG"
     val groupId = "dau_group"
     val inputDstream: InputDStream[ConsumerRecord[String, String]] = MyKafkaUtil.getKafkaStream(topic, ssc, groupId)
-    inputDstream.map(_.value()).print()
+    //    inputDstream.map(_.value()).print(100)
 
     // 3 统计用户当日的首次访问 dau uv
     //    1 可以通过判断 日志中page 栏位是否last_page_id来决定该页面 --> 一次访问会话的首个页面
     //    2 也可以通过启动日志来判断  是否首次访问
+    // 先转换格式，转换方便操纵的jsonObject
+    val logJspnDstream: DStream[JSONObject] = inputDstream.map { record =>
+      val jsonString: String = record.value()
+      val logJsonObj: JSONObject = JSON.parseObject(jsonString)
+      val ts: lang.Long = logJsonObj.getLong("ts")
+      val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH")
+      val dateHourString: String = simpleDateFormat.format(new Date(ts))
+      val dt: String = dateHourString.split(" ")(0)
+      val hr: String = dateHourString.split(" ")(1)
+      logJsonObj.put("dt", dt)
+      logJsonObj.put("hr", hr)
+      logJsonObj
+    }
 
-    // 要把访问会话次数 --> 当日的首次访问（会话）
+    // 过滤 得到每次会话的第一个访问页面
+    val firstPageJsonDstream: DStream[JSONObject] = logJspnDstream.filter { logJsonObj =>
+
+      var isFlagPage = false
+      val pageJsonObj: JSONObject = logJsonObj.getJSONObject("page")
+      if (pageJsonObj != null) {
+        val lastPageId: AnyRef = pageJsonObj.get("last_page_id")
+        if (lastPageId == null) {
+          isFlagPage = true
+        }
+      }
+      isFlagPage
+    }
+
+    //    firstPageJsonDstream.print(1000)
+
+    // 要把访问会话次数 --> 当日的首次访问（日活）
+    // 要如何去重：本质来说就是一种识别，识别每条日志对于当日来说是不是已经来过
+    // 如何保存用户访问清单 ： redis
+//    val dauJsonDstream: DStream[JSONObject] = firstPageJsonDstream.filter { logJsonObj =>
+//      val jedis = RedisUtil.getJedisClient // 转换成连接池连接，减少开辟连接的次数
+//      val dauKey = "dau:" + logJsonObj.getString("dt") // 设定key，每天一个清单，每个日期一个key
+//      val mid: String = logJsonObj.getJSONObject("common").getString("mid")
+//      val isFirstVisit: lang.Long = jedis.sadd(dauKey, mid)
+//      jedis.close()
+//      if (isFirstVisit == 1L) {
+//        println("用户：" + mid + "首次访问")
+//        true
+//      } else {
+//        println("用户：" + mid + "已经重复，去掉")
+//        false
+//      }
+//    }
+
+    val dauJsonDstream: DStream[JSONObject] = firstPageJsonDstream.mapPartitions { jsonItr =>
+
+      val jedis = RedisUtil.getJedisClient // 转换成连接池连接，减少开辟连接的次数
+      val dauList = new ListBuffer[JSONObject]
+      for (logJsonObj <- jsonItr) {
+        val dauKey = "dau:" + logJsonObj.getString("dt") // 设定key，每天一个清单，每个日期一个key
+        val mid: String = logJsonObj.getJSONObject("common").getString("mid")
+        val isFirstVisit: lang.Long = jedis.sadd(dauKey, mid)
+        if (isFirstVisit == 1L) {
+          println("用户：" + mid + "首次访问")
+          dauList.append(logJsonObj)
+        } else {
+          println("用户：" + mid + "已经重复，去掉")
+          false
+        }
+      }
+      jedis.close()
+      dauList.toIterator
+
+    }
+
+    dauJsonDstream.print(1000)
 
     ssc.start()
     ssc.awaitTermination()
