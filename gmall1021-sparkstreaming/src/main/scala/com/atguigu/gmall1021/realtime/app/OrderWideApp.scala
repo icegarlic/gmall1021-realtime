4b651f39d2b5144204280ded59e70b19a8895fca
zhaohan <577530562@qq.com>
2021-03-25 00:17:02 +0800

    update

--- gmall1021-sparkstreaming/src/main/scala/com/atguigu/gmall1021/realtime/app/OrderWideApp.scala
+++ gmall1021-sparkstreaming/src/main/scala/com/atguigu/gmall1021/realtime/app/OrderWideApp.scala
@@ -1,13 +1,18 @@
 package com.atguigu.gmall1021.realtime.app
 
 import com.alibaba.fastjson.{JSON, JSONObject}
-import com.atguigu.gmall1021.realtime.util.{MyKafkaUtil, OffsetManageUtil}
+import com.atguigu.gmall1021.realtime.util.{HbaseUtil, MyKafkaUtil, OffsetManageUtil}
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.TopicPartition
 import org.apache.spark.SparkConf
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.kafka010.{HasOffsetRanges, OffsetRange}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+import java.time.{LocalDate, Period}
+import java.util.Date
 
 object OrderWideApp {
 
@@ -61,13 +66,13 @@ object OrderWideApp {
 
     // 4 护具结构调整，把流中元素从record 调整为jsonObj
     // 主表
-    val orderInfoJsonObjDstream: DStream[AnyRef] = orderInfoWithOffsetDstream.map { record =>
+    val orderInfoJsonObjDstream: DStream[JSONObject] = orderInfoWithOffsetDstream.map { record =>
       val jsonString: String = record.value()
       val jsonObj: JSONObject = JSON.parseObject(jsonString)
       jsonObj
     }
     // 从表
-    val orderDetailJsonObjDstream: DStream[AnyRef] = orderDetailWithOffsetDstream.map { record =>
+    val orderDetailJsonObjDstream: DStream[JSONObject] = orderDetailWithOffsetDstream.map { record =>
       val jsonString: String = record.value()
       val jsonObj: JSONObject = JSON.parseObject(jsonString)
       jsonObj
@@ -76,8 +81,53 @@ object OrderWideApp {
     orderInfoJsonObjDstream.print(100)
     orderDetailJsonObjDstream.print(100)
 
-//    OffsetManageUtil.saveOffset(orderInfoTopic,groupId,orderInfoOffsetRanges)
-//    OffsetManageUtil.saveOffset(orderDetailTopic,groupId,orderDetailOffsetRanges)
+    val orderInfoObjWithUserDstream: DStream[JSONObject] = orderInfoJsonObjDstream.map { orderInfoJsonObj =>
+      // 需要补充用户字段
+      val userId: String = orderInfoJsonObj.getString("user_id")
+      val rowkey: String = HbaseUtil.getRowkey(userId)
+      val userJsonObj: JSONObject = HbaseUtil.get("DIM_USER_INFO", rowkey)
+      orderInfoJsonObj.put("user_gender", userJsonObj.getString("gender"))
+      val birthday: LocalDate = LocalDate.parse(userJsonObj.getString("birthday"))
+      val now: LocalDate = LocalDate.now()
+      val period: Period = Period.between(birthday, now)
+      val years: Int = period.getYears
+      orderInfoJsonObj.put("user_age", years)
+      orderInfoJsonObj
+    }
+
+    // 完成province
+    // 小表 考虑 直接把数据表从库中 加载到内存中，流中的数据逐个访问内存即可
+
+    val orderInfoWithProvinceUserDstream: DStream[JSONObject] = orderInfoObjWithUserDstream.transform { rdd =>
+      // driver 每批次执行一次
+      val provinceMap: Map[String, JSONObject] = HbaseUtil.getTableData("DIM_BASE_PROVINCE")
+      // 封装广播变量
+      val provinceMapBC: Broadcast[Map[String, JSONObject]] = ssc.sparkContext.broadcast(provinceMap)
+      val orderInfoJSONRdd: RDD[JSONObject] = rdd.map { orderInfoJsonobj =>
+        // 从订单流中获取订单的省市id
+        val provinceId: String = orderInfoJsonobj.getString("province_id")
+        // 把id转为rowkey
+        val rowkey: String = HbaseUtil.getRowkey(provinceId)
+        // 用rowkey从map中查询省市数据
+        // executor
+        // 从广播变量中提取数据
+        val provinceMapFromBC: Map[String, JSONObject] = provinceMapBC.value
+        val provinceObj: JSONObject = provinceMapFromBC.getOrElse(rowkey, null)
+        // 提取省市数据的字段值
+        if (provinceObj != null) {
+          val provinceName: String = provinceObj.getString("name")
+          // 补充到订单中
+          orderInfoJsonobj.put("province_name", provinceName)
+        }
+        orderInfoJsonobj
+      }
+      orderInfoJSONRdd
+    }
+
+    orderInfoWithProvinceUserDstream.print(100)
+
+    //    OffsetManageUtil.saveOffset(orderInfoTopic,groupId,orderInfoOffsetRanges)
+    //    OffsetManageUtil.saveOffset(orderDetailTopic,groupId,orderDetailOffsetRanges)
 
     ssc.start()
     ssc.awaitTermination()
--- gmall1021-sparkstreaming/src/main/scala/com/atguigu/gmall1021/realtime/util/HbaseUtil.scala
+++ gmall1021-sparkstreaming/src/main/scala/com/atguigu/gmall1021/realtime/util/HbaseUtil.scala
@@ -16,27 +16,27 @@ object HbaseUtil {
 
   private val properties: Properties = PropertiesUtil.load("config.properties")
 
-  val DEFAULT_NAMESPACE="GMALL1021"
-  val DEFAULT_FAMILY="INFO"
-  val HBASE_SERVER=properties.getProperty("hbase.server")
+  val DEFAULT_NAMESPACE = "GMALL1021"
+  val DEFAULT_FAMILY = "INFO"
+  val HBASE_SERVER = properties.getProperty("hbase.server")
 
-  var connection:Connection=null
+  var connection: Connection = null
 
   /*
   写入方法
    */
-  def put(tableName:String,rowKey:String,dataJsonObj:JSONObject): Unit ={
-    if(connection==null) init()  // 判断有没有连接 如果米有创建
+  def put(tableName: String, rowKey: String, dataJsonObj: JSONObject): Unit = {
+    if (connection == null) init() // 判断有没有连接 如果米有创建
     //从连接中获得表对象
-    val table: Table = connection.getTable(TableName.valueOf(DEFAULT_NAMESPACE+":"+tableName))
-    val putList:util.List[Put] = new util.ArrayList[Put]()
+    val table: Table = connection.getTable(TableName.valueOf(DEFAULT_NAMESPACE + ":" + tableName))
+    val putList: util.List[Put] = new util.ArrayList[Put]()
     //组合多个put, 每个put代表提交一个字段
     //用什么进行循环
-    import  collection.JavaConverters._
-    for (entry <- dataJsonObj.entrySet().asScala ) {
+    import collection.JavaConverters._
+    for (entry <- dataJsonObj.entrySet().asScala) {
       val columnName: String = entry.getKey
-      var columnValue: String=""
-      if(entry.getValue!=null){
+      var columnValue: String = ""
+      if (entry.getValue != null) {
         columnValue = entry.getValue.toString
         //创建put动作
 
@@ -54,12 +54,15 @@ object HbaseUtil {
 
   }
 
-  /*
-  查询方法  按rowkey查询
+  /**
+   * 查询方法
+   * @param tableName
+   * @param rowkey
+   * @return
    */
-  def  get(tableName:String,rowkey:String): JSONObject ={
-    if(connection==null) init()  // 判断有没有连接 如果米有创建
-    val table: Table = connection.getTable(TableName.valueOf(DEFAULT_NAMESPACE+":"+tableName))
+  def get(tableName: String, rowkey: String): JSONObject = {
+    if (connection == null) init() // 判断有没有连接 如果米有创建
+    val table: Table = connection.getTable(TableName.valueOf(DEFAULT_NAMESPACE + ":" + tableName))
 
     val get = new Get(Bytes.toBytes(rowkey))
     val result: Result = table.get(get)
@@ -68,54 +71,55 @@ object HbaseUtil {
 
   /**
    * 整表查询
+   *
    * @param tableName
-   * @return  map  key=rowkey  ，value 字段和值
+   * @return map  key=rowkey  ，value 字段和值
    */
-  def getTableData(tableName:String): Map[String ,JSONObject] ={
-    if(connection==null) init()  // 判断有没有连接 如果米有创建
-    val table: Table = connection.getTable(TableName.valueOf(DEFAULT_NAMESPACE+":"+tableName))
+  def getTableData(tableName: String): Map[String, JSONObject] = {
+    if (connection == null) init() // 判断有没有连接 如果米有创建
+    val table: Table = connection.getTable(TableName.valueOf(DEFAULT_NAMESPACE + ":" + tableName))
 
     val scan = new Scan
     val resultScanner: ResultScanner = table.getScanner(scan)
-    val resultList: ListBuffer[(String, JSONObject)] = ListBuffer[(String,JSONObject)]()
-    import  collection.JavaConverters._
+    val resultList: ListBuffer[(String, JSONObject)] = ListBuffer[(String, JSONObject)]()
+    import collection.JavaConverters._
     //相当于把每行的数据进行提取转换
-    for (result <- resultScanner.iterator().asScala ) {
+    for (result <- resultScanner.iterator().asScala) {
       val jsonObj: JSONObject = convertToJsonObj(result)
       val rowkey: String = Bytes.toString(result.getRow)
-      resultList.append((rowkey,jsonObj))
+      resultList.append((rowkey, jsonObj))
     }
     resultList.toMap
   }
 
   //把result转换成jsonObj
-  def  convertToJsonObj(result: Result): JSONObject ={
+  def convertToJsonObj(result: Result): JSONObject = {
     val cells: Array[Cell] = result.rawCells()
     //每个cell就是一个col-value
     val jsonObj = new JSONObject()
-    for (cell <- cells ) {
+    for (cell <- cells) {
       //通过工具类从cell中提取 列名和列值
-      val colName: String = Bytes.toString( CellUtil.cloneQualifier(cell))
-      val colValue: String = Bytes.toString( CellUtil.cloneValue(cell))
-      jsonObj.put(colName,colValue)
+      val colName: String = Bytes.toString(CellUtil.cloneQualifier(cell))
+      val colValue: String = Bytes.toString(CellUtil.cloneValue(cell))
+      jsonObj.put(colName, colValue)
     }
     jsonObj
   }
 
 
-  def init(): Unit ={
+  def init(): Unit = {
     //创建配置类
     val configuration: Configuration = HBaseConfiguration.create()
     //设定地址
-    configuration.set("hbase.zookeeper.quorum",HBASE_SERVER)
+    configuration.set("hbase.zookeeper.quorum", HBASE_SERVER)
     //创建连接
-    connection=ConnectionFactory.createConnection(configuration)
+    connection = ConnectionFactory.createConnection(configuration)
 
   }
 
   //统一规则生成 rowkey
-  def getRowkey(id:String): String ={
-    var rowkey= StringUtils.leftPad(id.toString,10,"0").reverse
+  def getRowkey(id: String): String = {
+    var rowkey = StringUtils.leftPad(id.toString, 10, "0").reverse
     rowkey
   }
 
